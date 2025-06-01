60a259fdac3bb19121933bade59fc4fd6517b6ad
Sofiia Shevkoplias <sofiashevkoplias@gmail.com>
2025-06-01 14:47:37 +0200

    14 view all

--- src/screens/HubsScreen.js
+++ src/screens/HubsScreen.js
@@ -398,7 +398,7 @@ const styles = StyleSheet.create({
     },
 
     text_view_all: {
-        fontSize: 12,
+        fontSize: 14,
         fontFamily: 'Poppins_600SemiBold',
         color: colors.blueTertiary
     },
