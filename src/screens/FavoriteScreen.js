ee4362449d42b56ba2ec74cacebce66ef47fd3fd
Ivan Hriazin <a12233667@unet.univie.ac.at>
2025-06-01 14:20:25 +0200

    fixeof fonts and etc

--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -65,7 +65,7 @@ const styles = StyleSheet.create({
     },
     itemSubtitle: {
         fontSize: 14,
-        fontFamily: 'Poppins_600SemiBold',
+        fontFamily: 'Poppins_500Medium',
         color: colors.bluePrimary,
     },
     noFavoritesText: {
